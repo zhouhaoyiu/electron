9d3f1d7e3fc395857cf73fb13688a4a088d2aacc
云香绕雪 <67226385+zhouhaoyiu@users.noreply.github.com>
2025-03-31 20:42:22 +0800

    add vue

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
+// import Vue from 'vue';
+
+// import App from './App.vue';
+
+
+// new Vue({
+
+
+//   render: h => h(App),
+
+
+// }).$mount('#app');
+
+
 const func = async () => {
     const response = await window.versions.ping()
     console.log(response) // prints out 'pong'
